eb2f0e6c5f9a6c06f38e8beeb32866b59457da95
Travis Glenn Hansen <travisghansen@yahoo.com>
2010-08-26 18:28:12 +0000

    ready for testing

--- public/index.php
+++ public/index.php
@@ -12,8 +12,11 @@ write_log($message);
 //$filename
 $accounts_ini = parse_ini_file(CONFIG_DIR . DIRECTORY_SEPARATOR . 'accounts.ini', true);
 $account_ini = $accounts_ini[$from];
-var_dump($accounts_ini);
-var_dump($account_ini);
+
+$config_ini = parse_ini_file(CONFIG_DIR . DIRECTORY_SEPARATOR . 'config.ini', true);
+
+//var_dump($accounts_ini);
+//var_dump($account_ini);
 
 //Get all account info
 $jid = $account_ini['jid'];
@@ -32,12 +35,15 @@ include 'XMPPHP/XMPP.php';
 #If this doesn't work, are you running 64-bit PHP with < 5.2.6?
 $conn = new XMPPHP_XMPP($host, $port, $username, $password, $resource, $server, $printlog=false, $loglevel=XMPPHP_Log::LEVEL_INFO);
 
+$to_jid = $config_ini['debug']['to_jid'];
+
 try {
 	$conn->connect();
 	$conn->processUntil('session_start');
 	$conn->presence();
-	$conn->message('thansen@one-gear.com', 'This is a test message!');
+	$conn->message($to_jid, $text);
 	$conn->disconnect();
+	echo "ok";
 } catch(XMPPHP_Exception $e) {
 	die($e->getMessage());
 }
